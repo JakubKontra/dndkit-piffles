b4b433632f4a7fa88cccb68c35ddfec09006f22f
Jakub Kontra <me@jakubkontra.com>
2022-05-19 11:37:57 +0200

    initial commit

---
+++ components/Builder/Builder.tsx
@@ -0,0 +1,3 @@
+import { MultipleContainers } from './MultipleContainers';
+
+export const Builder = () => <MultipleContainers itemCount={5} vertical />;
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Builder } from '../components/Builder/Builder'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+    <Builder />
+    </div>
+  )
+}
+
+export default Home
